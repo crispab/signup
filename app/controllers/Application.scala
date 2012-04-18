62d62a9ba9d00f52e30fef47da0ad6a9d93769f9
Mats Strandberg <mats@strandberg.net>
2012-04-18 20:59:16 +0100

    First simple database setup working

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,19 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models.User
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val user = new User(null, "Nisse")
+    User.create(user)
+    Ok(views.html.index("Created user"))
+  }
+  
+  def show = Action {
+    val users = User.findAll()
+    Ok(views.html.index("Found users: " + users))
   }
   
 }
