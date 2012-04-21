49c94f26549fe759731978e7f8568503b75149c5
Mats Strandberg <mats@strandberg.net>
2012-04-21 14:53:50 +0200

    Fixed a new evolution which seems to be needed for Heroku

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,10 @@
+# --- First database schema
+# --- For heroku deployment - use PostgreSQL - see http://www.postgresql.org/
+
+# --- !Ups
+ 
+INSERT INTO users (name) VALUES ('Göran');
+
+ 
+# --- !Downs
+ 
