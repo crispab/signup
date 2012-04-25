43432923788751d69349a31437cc6e4c09d9f183
Mats Strandberg <mats@strandberg.net>
2012-04-25 20:36:11 +0200

    Simple post of create user form

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -2,17 +2,13 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms.{tuple, nonEmptyText, text}
 import models.User
 import anorm.NotAssigned
 
 object Users extends Controller {
-  
-  def create = Action {
-    val user = new User(NotAssigned, "Nisse")
-    User.create(user)
-    Ok(views.html.index("Created user"))
-  }
-  
+    
   def list = Action {
     val users = User.findAll()
     Ok(views.html.users_list("Found users: " + users))
@@ -20,9 +16,28 @@ object Users extends Controller {
   
   def createForm = Action {
     val user = new User(NotAssigned, "")
-    Ok(views.html.users_editForm(user))
+    Ok(views.html.users.edit(user, newUser = true))
   }
   
-
+  def create = Action { implicit request =>
+    userForm.bindFromRequest.fold(
+      errors => BadRequest,
+      {
+        case (firstName, nickName, lastName) => {
+          User.create(User(NotAssigned, firstName=firstName, nickName=nickName, lastName=lastName))         
+          Redirect(routes.Users.list)
+        }
+      }        
+    )
+  }
+    
+  val userForm = Form(
+	tuple(
+	  "firstName" -> nonEmptyText,
+      "nickName" -> text,
+      "lastName" -> text
+	)
+  )
   
 }
+
