44ceca0d011e96a77c0a8335ebb386975383f2a6
Jan Grape <jan.grape@crisp.se>
2012-06-09 18:11:34 +0200

    Trying to make evolution scripts compatible with whatever runs on Heroku

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -12,7 +12,7 @@ ALTER TABLE events ADD COLUMN end_time TIMESTAMP;
 UPDATE events SET end_time=whenx;
 ALTER TABLE events ALTER COLUMN end_time SET NOT NULL;
 
-ALTER TABLE events DROP COLUMN IF EXISTS whenx;
+ALTER TABLE events DROP COLUMN whenx;
 
 # --- !Downs
  
@@ -20,5 +20,5 @@ ALTER TABLE events ADD COLUMN whenx TIMESTAMP;
 UPDATE events SET whenx=start_time;
 ALTER TABLE events ALTER COLUMN whenx SET NOT NULL;
 
-ALTER TABLE events DROP COLUMN IF EXISTS start_time;
-ALTER TABLE events DROP COLUMN IF EXISTS end_time;
+ALTER TABLE events DROP COLUMN start_time;
+ALTER TABLE events DROP COLUMN end_time;
--- conf/evolutions/default/5.sql
+++ conf/evolutions/default/5.sql
@@ -8,27 +8,27 @@ ALTER TABLE users ALTER COLUMN last_name SET NOT NULL;
 
 ALTER TABLE users ADD COLUMN email VARCHAR(127);
 UPDATE users SET email=primary_email;
-ALTER TABLE users DROP COLUMN IF EXISTS primary_email;
+ALTER TABLE users DROP COLUMN primary_email;
 
 UPDATE users SET email='unknown@crisp.se' where email IS NULL OR email = '';
 ALTER TABLE users ALTER COLUMN email SET NOT NULL;
 
 ALTER TABLE users ADD COLUMN phone VARCHAR(127) DEFAULT '';
 UPDATE users SET phone=mobile_nr;
-ALTER TABLE users DROP COLUMN IF EXISTS mobile_nr;
+ALTER TABLE users DROP COLUMN mobile_nr;
 
-ALTER TABLE users DROP COLUMN IF EXISTS nick_name;
-ALTER TABLE users DROP COLUMN IF EXISTS secondary_email;
+ALTER TABLE users DROP COLUMN nick_name;
+ALTER TABLE users DROP COLUMN secondary_email;
 
 # --- !Downs
 
 ALTER TABLE users ADD COLUMN primary_email VARCHAR(127) DEFAULT '';
 UPDATE users SET primary_email = email;
-ALTER TABLE users DROP COLUMN IF EXISTS email;
+ALTER TABLE users DROP COLUMN email;
 
 ALTER TABLE users ADD COLUMN mobile_nr VARCHAR(127) DEFAULT '';
 UPDATE users SET mobile_nr=phone;
-ALTER TABLE users DROP COLUMN IF EXISTS phone;
+ALTER TABLE users DROP COLUMN phone;
 
 ALTER TABLE users ADD COLUMN nick_name VARCHAR(127) DEFAULT '';
 ALTER TABLE users ADD COLUMN secondary_email VARCHAR(127) DEFAULT '';
