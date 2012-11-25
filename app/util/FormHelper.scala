1361abf61e18f4507e1d0be8562865951c6f2e1b
Jan Grape <jan.grape@crisp.se>
2012-11-25 03:40:44 +0100

    Added Gravatar to top bar when logged in

--- app/util/FormHelper.scala
+++ app/util/FormHelper.scala
@@ -1,10 +1,12 @@
 package util
 
 import play.api.data.Form
+import play.api.data.FormError
 
 object FormHelper {
   def errors[T](form: Form[T]) : Seq[String] = {
-    val messages:Seq[String] = form.errors.map(e => e.message + ": " + e.key).union(form.globalErrors.map(e => e.message + ": " + e.key))
+    val errors: scala.Seq[FormError] = form.errors union form.globalErrors
+    val messages:Seq[String] = errors.map(e => e.message + ": " + e.key)
     messages
   }
 }
