23368d6ebf18114fd0f6240b6648219c2ff8b444
Jan Grape <jan.grape@crisp.se>
2013-03-28 18:45:17 +0100

    Fixed #40, Passwords salted now

--- app/util/AuthHelper.scala
+++ app/util/AuthHelper.scala
@@ -21,14 +21,17 @@ object AuthHelper {
     user.permission == permission
   }
 
-  def checkPassword(user : Option[User], password: String): Option[User] = {
-    user.filter(usr => usr.password.toLowerCase == md5(password))
+  def checkPassword(user : Option[User], enteredPassword: String): Option[User] = {
+    user.filter(usr => usr.password.toLowerCase == calculateHash(enteredPassword))
   }
 
 
-  def md5(s: String): String = {
-    val hash = MessageDigest.getInstance("MD5").digest(s.getBytes).map("%02X".format(_)).mkString.toLowerCase
+  def calculateHash(password: String): String = {
+    val hash = MessageDigest.getInstance("MD5").digest((password + salt).getBytes).map("%02X".format(_)).mkString.toLowerCase
     hash
   }
+
+  import play.api.Play.current
+  private def salt = play.api.Play.configuration.getString("password.salt").getOrElse("")
 
 }
