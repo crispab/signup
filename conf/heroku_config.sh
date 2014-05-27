44c089af789e09203839012b1eab9f7fb810eef5
Jan Grape <jan.grape@crisp.se>
2014-05-27 21:29:07 +0200

    Setting time zone in Heroku config (cherry picked from commit 8ce4113)

--- conf/heroku_config.sh
+++ conf/heroku_config.sh
@@ -1,10 +1,13 @@
 heroku config:set \
     DATABASE_DRIVER=org.postgresql.Driver \
     SMTP_MOCK=false \
-    SMTP_HOST=[host] \
-    SMTP_PORT=25 \
+    SMTP_HOST=smtp.sendgrid.net \
+    SMTP_PORT=587 \
     SMTP_SSL=true \
     SMTP_TLS=true \
-    SMTP_USER=[user] \
-    SMTP_PASSWORD=[password] \
+    SMTP_USER=crisp \
+    SMTP_PASSWORD=pwd \
+    PASSWORD_SALT=salt \
+    NEW_RELIC_LOG=stdout \
+    TZ="Europe/Stockholm" \
     $*
