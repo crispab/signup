81938b717d75a87f5cbec87961aa3333d70cdc08
Jan Grape <jan.grape@crisp.se>
2014-06-13 12:35:53 +0200

    Better handling of whitespace when stripping HTML tags

--- app/util/HtmlHelper.scala
+++ app/util/HtmlHelper.scala
@@ -4,10 +4,12 @@ object HtmlHelper {
   def NON_APLHA_AND_SOME_REGEXP = "[^a-z0-9\\-_]"
 
   def stripFromHtml(htmlString: String): String = {
-    htmlString.replaceAll("<[Bb][Rr]>", "\n")
-              .replaceAll("<\\/[Pp]>", "\n")
-              .replaceAll("<\\/[Ll][Ii]>", "\n")
+    htmlString.replaceAll("\\n", " ")
               .replaceAll("<[Ll][Ii]>", " * ")
+              .replaceAll("\\s+", " ")
+              .replaceAll("<[Bb][Rr]>", "\n")
+              .replaceAll("<\\/[Pp]>", "\n\n")
+              .replaceAll("<\\/[Ll][Ii]>", "\n")
               .replaceAll("(?s)<[^>]*>(\\s*<[^>]*>)*", "")
   }
 
--- test/util/HtmlHelperSpec.scala
+++ test/util/HtmlHelperSpec.scala
@@ -10,16 +10,20 @@ class HtmlHelperSpec extends Specification {
       HtmlHelper.stripFromHtml("<span>Hej hopp! </span><div class='fnutt'>Grön...</DIV>") must beEqualTo("Hej hopp! Grön...")
     }
 
-    "replace </p> with new line" in {
-      HtmlHelper.stripFromHtml("<p>Hej hopp!</p><div class='fnutt'>Grön...</DIV>") must beEqualTo("Hej hopp!\nGrön...")
+    "replace </p> with double newline" in {
+      HtmlHelper.stripFromHtml("<p>Hej hopp!</p><div class='fnutt'>Grön...</DIV>") must beEqualTo("Hej hopp!\n\nGrön...")
     }
 
-    "replace <br> with new line" in {
+    "replace <br> with newline" in {
       HtmlHelper.stripFromHtml("Hej hopp!<bR><div class='fnutt'>Grön...</DIV>") must beEqualTo("Hej hopp!\nGrön...")
     }
 
     "replace <li> with ' * ' and </li> with new line" in {
       HtmlHelper.stripFromHtml("<ul><li>Hej hopp!</li><li>Grön...</li></ul>") must beEqualTo(" * Hej hopp!\n * Grön...\n")
+    }
+
+    "remove newlines from HTML" in {
+      HtmlHelper.stripFromHtml("<ul>\n\n<li>\nHej\n\n\nhopp!</li>\n<li>Grön...\n\n</li></ul>") must beEqualTo(" * Hej hopp!\n * Grön... \n")
     }
   }
 }
