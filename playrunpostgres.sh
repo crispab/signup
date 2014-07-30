e84325a1d1229551a53da0913fec095f1231e6d1
Jan Grape <jan.grape@crisp.se>
2014-07-31 00:55:29 +0200

    Cleaned out some commented code

--- playrunpostgres.sh
+++ playrunpostgres.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-# Start play overriding database config with config for PostgreSQL (rather than H2)
-
-# export SMTP_MOCK=false
-# export SMTP_HOST=<host>
-# export SMTP_PORT=25
-# export SMTP_SSL=true
-# export SMTP_TLS=true
-# export SMTP_USER=<user>
-# export SMTP_PASSWORD=<password>
-
 play debug -Dconfig.file=conf/postgres.conf run
 
