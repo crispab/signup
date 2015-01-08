28cddb9aa67299f680b9f790d306305e56c80927
Jan Grape <jan.grape@crisp.se>
2015-01-08 21:41:17 +0100

    Converted some old test cases

---
+++ test/unit/services/ImageUrlTest.scala
@@ -0,0 +1,34 @@
+package unit.services
+
+import models.User
+import org.scalatestplus.play._
+import services.{GravatarUrl, ImageUrl}
+
+class ImageUrlTest extends PlaySpec {
+
+  def CORRECT_URL = "https://secure.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346.jpg?default=blank&size=40"
+
+  "ImageUrl object " must {
+
+    "handle simple case" in {
+      val user = User(firstName = "Hari", lastName = "Seldon", email = "myemailaddress@example.com", imageProvider = GravatarUrl.identifier)
+      ImageUrl(user) must equal(CORRECT_URL)
+    }
+
+    "handle CamelCase" in {
+      val user = User(firstName = "Hari", lastName = "Seldon", email = "MyEmailAddress@example.com", imageProvider = GravatarUrl.identifier)
+      ImageUrl(user) must equal(CORRECT_URL)
+    }
+
+    "handle white space" in {
+      val user = User(firstName = "Hari", lastName = "Seldon", email = "  MyEmailAddress@example.com ", imageProvider = GravatarUrl.identifier)
+      ImageUrl(user) must equal(CORRECT_URL)
+    }
+
+    "handle empty string" in {
+      val user = User(firstName = "Hari", lastName = "Seldon", email = "", imageProvider = GravatarUrl.identifier)
+      ImageUrl(user).size must be > 0
+    }
+  }
+}
+
