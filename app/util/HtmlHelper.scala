309392d3b7e2d7ac304b497fbd270539a3819dfe
Jan Grape <jan.grape@crisp.se>
2015-04-02 14:27:07 +0200

    Added http-link in calendar description

--- app/util/HtmlHelper.scala
+++ app/util/HtmlHelper.scala
@@ -1,6 +1,7 @@
 package util
 
-import models.Status
+import com.sun.org.glassfish.gmbal.Description
+import models.{Event, Status}
 
 object HtmlHelper {
   def NON_APLHA_AND_SOME_REGEXP = "[^a-z0-9\\-_]"
@@ -36,6 +37,17 @@ object HtmlHelper {
     }
   }
 
+  def truncatedTextFromHtml(htmlDescription: String, maxlength:Int): String = {
+    val description = stripFromHtml(htmlDescription)
+    if(description.length > maxlength)
+      description.substring(0, maxlength) + "..."
+    else
+      description
+  }
 
-
+  def calendarDescriptionAsText(event: Event, url: String, maxlength: Int): String = {
+    import play.api.Play.current
+    val baseUrl = play.api.Play.configuration.getString("application.base.url").getOrElse("")
+    truncatedTextFromHtml("<p>Sammankomst: " + baseUrl + url + "</p>" + event.description, maxlength)
+  }
 }
