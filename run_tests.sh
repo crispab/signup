11bd1363f39db5ea21bb737b7e68d05ca1b74313
Jan Grape <jan.grape@crisp.se>
2017-05-13 10:21:47 +0200

    Run *all* the tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ export WEB_DRIVER_TYPE=phantomjs
 export PHANTOMJS_BINARRY_PATH=/usr/local/bin/phantomjs
 export APPLICATION_BASE_URL=http://localhost:3333
 
-activator 'test-only acceptance.*'
+activator 'test-only unit.* integration.* acceptance.*'
