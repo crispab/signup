9fb27386523977a058f0600b537d23d8ca219115
Jan Grape <jan.grape@crisp.se>
2017-10-19 20:47:07 +0200

    Updated instructions in script

--- docker_run_dev_db.sh
+++ docker_run_dev_db.sh
@@ -6,7 +6,10 @@
 
 # This launches a PostgreSQL database with the correct user, password and database
 # name as a detached Docker container.
+docker run --name postgres -e POSTGRES_USER=signup4 -e POSTGRES_PASSWORD=s7p2+ -e POSTGRES_DB=signup -p 5432:5432 -d postgres:9
+
+# Some nice docker commands:
 # Check with 'docker ps'
 # Stop with 'docker stop postgres'
+# Start with 'docker start postgres'
 # Delete with 'docker rm postgres'
-docker run --name postgres -e POSTGRES_USER=signup4 -e POSTGRES_PASSWORD=s7p2+ -e POSTGRES_DB=signup -p 5432:5432 -d postgres:9
