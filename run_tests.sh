904032a3238360d5e4f52ef22a45d078c78139c0
Jan Grape <jan.grape@crisp.se>
2018-03-12 23:42:25 +0100

    Parameter test-only => testOnly (SBT 1.x style)

--- run_tests.sh
+++ run_tests.sh
@@ -5,4 +5,4 @@ export APPLICATION_BASE_URL=http://localhost:19001
 
 export LANG=sv_SE
 
-sbt -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver 'test-only se.crisp.signup4.unit.* se.crisp.signup4.integration.* se.crisp.signup4.acceptance.*'
+sbt -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver 'testOnly se.crisp.signup4.unit.* se.crisp.signup4.integration.* se.crisp.signup4.acceptance.*'
